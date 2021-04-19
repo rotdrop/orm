07ee12b1f8952a8b6bbf17ee65982d30664a3534
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-04-19 13:36:07 +0200

    SchemaTool: IMHO join-columns should not inherit the default value from the referenced column.

--- lib/Doctrine/ORM/Tools/SchemaTool.php
+++ lib/Doctrine/ORM/Tools/SchemaTool.php
@@ -719,6 +719,7 @@ class SchemaTool
                 }
 
                 $columnOptions += $this->gatherColumnOptions($fieldMapping);
+                unset($columnOptions['default']);
 
                 if ($fieldMapping['type'] === 'string' && isset($fieldMapping['length'])) {
                     $columnOptions['length'] = $fieldMapping['length'];
