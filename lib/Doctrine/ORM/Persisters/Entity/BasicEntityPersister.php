c3727a9bcf7a1475c05dcd82444a5fac8e7cbf3e
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-03 17:12:30 +0200

    Protect auto-increment columns used in associations during insert.
    When inserting an entity with an to-one association where one
    join-column is an auto-increment key then this column should not be
    inserted.

--- lib/Doctrine/ORM/Persisters/Entity/BasicEntityPersister.php
+++ lib/Doctrine/ORM/Persisters/Entity/BasicEntityPersister.php
@@ -1498,6 +1498,7 @@ class BasicEntityPersister implements EntityPersister
     protected function getInsertColumnList()
     {
         $columns = [];
+        $autoColumn = $this->class->isIdGeneratorIdentity() ? $this->class->identifier[0] : '';
 
         foreach ($this->class->reflFields as $name => $field) {
             if ($this->class->isVersioned && $this->class->versionField === $name) {
@@ -1513,14 +1514,16 @@ class BasicEntityPersister implements EntityPersister
 
                 if ($assoc['isOwningSide'] && $assoc['type'] & ClassMetadata::TO_ONE) {
                     foreach ($assoc['joinColumns'] as $joinColumn) {
-                        $columns[] = $this->quoteStrategy->getJoinColumnName($joinColumn, $this->class, $this->platform);
+                        if ($autoColumn !== $joinColumn['name']) {
+                            $columns[] = $this->quoteStrategy->getJoinColumnName($joinColumn, $this->class, $this->platform);
+                        }
                     }
                 }
 
                 continue;
             }
 
-            if (! $this->class->isIdGeneratorIdentity() || $this->class->identifier[0] !== $name) {
+            if ($autoColumn !== $name) {
                 if (isset($this->class->fieldMappings[$name]['notInsertable'])) {
                     continue;
                 }
