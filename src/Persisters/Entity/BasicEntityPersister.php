ad0c6a9144667807ba5feec7f1cbf01eeda9c040
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-03 11:15:39 +0200

    Protect id-columns used in associations against null during update.
    When trying to set an to-one association to null when the join-columns
    involve id-columns of the source-entity the BasicEntityPersister would
    just set all source-columns to null. This seems to be not desirable when
    some of the join columns refer to identifier columns of the source
    entity.

--- src/Persisters/Entity/BasicEntityPersister.php
+++ src/Persisters/Entity/BasicEntityPersister.php
@@ -590,6 +590,7 @@ class BasicEntityPersister implements EntityPersister
     protected function prepareUpdateData(object $entity, bool $isInsert = false): array
     {
         $joinColumnUsage = null;
+        $idColumns    = null;
         $versionField = null;
         $result       = [];
         $uow          = $this->em->getUnitOfWork();
@@ -703,6 +704,20 @@ class BasicEntityPersister implements EntityPersister
 
                 if (empty($newColumnValue) && $joinColumnUsage[$sourceColumn] > 0) {
                     continue; // still in use, avoid overwriting it
+                }
+
+                if (empty($idColumns)) {
+                    $idColumns = $this->class->getIdentifierColumnNames();
+                }
+
+                if (array_search($sourceColumn, $idColumns) !== false) {
+                  // Remove the identifier column from the update if
+                  // this is just an attempt to set the association to
+                  // null. Still it is not a good idea to change an
+                  // identifier value with an update statement ...
+                  if (empty($newValId)) {
+                    continue;
+                  }
                 }
 
                 if (!empty($result[$owningTable][$sourceColumn])) {
